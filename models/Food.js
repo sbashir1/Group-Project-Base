714eb2c256907c67af7977b5d1a8755477636e8e
Julia Escarda <78128989+jaescarda@users.noreply.github.com>
2021-04-14 22:21:10 -0400

    fixed food model

--- models/Food.js
+++ models/Food.js
@@ -1,17 +1,20 @@
 export default (sequelize, DataTypes) => {
   const Food = sequelize.define(
-    'Food',
+    'food',
     {
       food_id: {
         type: DataTypes.INTEGER,
-        allowNull: false
+        allowNull: false,
+        primaryKey: true,
+        unique: true
       },
       food_type: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: true
       }
     },
     { freezeTableName: true, timestamps: false }
   );
   return Food;
 };
+
