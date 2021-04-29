95a547ecfc823d5f8309f60a2b10ddef1b80665d
gar622 <73448556+gar622@users.noreply.github.com>
2021-04-29 01:52:48 -0600

    monument js
    popluated the boxes! but no pictures:/ -- yet!

--- public/monuments/monuTestScript.js
+++ public/monuments/monuTestScript.js
@@ -1,25 +1,3 @@
-async function populateMonuments(){
-    console.log('data request');
-    const monuRequest = await fetch('/api/monuments')
-    const monuData = await monuRequest.json();
-
-    monuData.data.forEach((monument) => {
-        const appendItem = document.createElement("div");
-        appendItem.classList.add("tile", "had-text-centered", "is-parent", "is-3");
-        appendItem.innerHTML = `
-        <article class = "tie is-child box has-background-link-dark ">
-        <span class = "subtitle has-text-ight has-text-weight-bold">
-        ${monument["Monument_name"]}</span>
-        <br />
-        <span class = "has-text-light">
-        ${monument.Monument_address.split(",")[0]}</span>
-        <br/>
-        <span class = "has-text-light">
-        ${monument["Monument_zip"]}</span>
-        </article>`;
-        targetBox.append(appendItem)
-    });
-} 
 async function getMonuments(){
     console.log('data request');
     const monuRequest = await fetch ('/api/monuments');
@@ -28,7 +6,26 @@ async function getMonuments(){
 }
 async function windowActions(){
     console.log('data request');
-    const data = await getMonuments();
-    console.table(data);
+    const monuInfo = await getMonuments();
+    console.table(monuInfo);
+
+    const monuList = document.querySelector('.box');
+    const html = monuInfo.map(place => {
+        console.log('display')
+
+        const monuName = place.Monument_name;
+        return `
+        <div class="box">  
+            <ul>
+                <li>
+                    <div class="Rname">${monuName}</div>
+                    ${place.Monument_address}<br>
+                    ${place.Monument_zip}<br>
+                </li>
+            </ul>
+        </div>
+        `;
+    }).join('');
+    monuList.innerHTML = html;
     }
 window.onload = windowActions;
