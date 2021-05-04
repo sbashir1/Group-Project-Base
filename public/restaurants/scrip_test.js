28cd9961ab0ec750143ff3c6d59dfca7e80d6880
Jonghyeong Kim <aengida@gmail.com>
2021-05-04 10:43:35 -0400

    Update test.js

--- public/restaurants/scrip_test.js
+++ public/restaurants/scrip_test.js
@@ -35,73 +35,7 @@ async function windowActions() {
         `;
   }).join('');
   restList.innerHTML = html;
-
-  // For search bar
-  console.log('search');
-  const request = await fetch('/api/restaurant_info');
-  const data = await request.json();
-
-  function findMatches(wordToMatch, data) {
-    console.log('find matches');
-    return data.filter((place) => {
-      const regex = new RegExp(wordToMatch, 'gi');
-      return place.restaurant_street.match(regex) || place.restaurant_name.match(regex);
-    });
-  }
-
-  function displayMatches(event) {
-    console.log('display');
-    const matchArray = findMatches(event.target.value, data);
-    const html = matchArray.map((place) => {
-      const restName = place.restaurant_name;
-
-      return `
-                <div class="box2">
-                    <ul>
-                        <li>
-                            <div class="Rname">${restName}</div>
-                            ${place.restaurant_street}
-                            ${place.restaurant_town}
-                            ${place.restaurant_zip}<br>
-                            ${place.restaurant_phone}<br>
-                            ${place.restaurant_email}<br>
-                        </li>
-                    </ul>
-                </div>
-            `;
-    }).join('');
-    suggestions.innerHTML = html;
-  }
-  const searchInput = document.querySelector('.userform');
-  const suggestions = document.querySelector('.suggestions');
-
-  searchInput.addEventListener('change', displayMatches);
-  searchInput.addEventListener('keyup', (evt) => {
-    evt.preventDefault();
-    displayMatches(evt);
-  });
 }
-async function awardsActions() {
-  const awardResults = await getAwards();
-  console.table(awardResults);
-
-  const awardList = document.querySelector('.awards-section');
-  const html = awardResults.map((awar) => {
-    console.log('display');
-    return `
-        <div class="box2">  
-            <ul>
-                <li>
-                    ${awar.Awards_ID}<br>
-                    ${awar.Award_name}<br>
-                </li>
-            </ul>
-        </div>
-        `;
-  }).join('');
-  awardList.innerHTML = html;
-}
-
 // Same search bar code from Home page
 async function restaurant() {
   console.log('Window loaded');
@@ -198,44 +132,71 @@ async function searchResults() {
 }
 
 window.onload = searchResults;
-
-//window.onload = windowActions;
+window.onload = windowActions;
 //window.onload = awardsActions;
 
-// PRACTICE STUFF BELOW HERE.
+// /// For search bar
+// console.log('search');
+// const request = await fetch('/api/restaurant_info');
+// const data = await request.json();
 
-// function getRandomIntInclusive(min, max) {
-//     min = Math.ceil(min);
-//     max = Math.floor(max);
-//     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+// function findMatches(wordToMatch, data) {
+//   console.log('find matches');
+//   return data.filter((place) => {
+//     const regex = new RegExp(wordToMatch, 'gi');
+//     return place.restaurant_street.match(regex) || place.restaurant_name.match(regex);
+//   });
 // }
 
-// const restArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// const selectedRest = restArray.map((element) => {
-//     const random = getRandomIntInclusive(0, restInfo.length -1);
-//     return restInfo[random];
-// });
-// console.table(selectedRest)
-
-// restInfo.forEach((place) => {
-//     console.log('display')
-//     const restList = document.querySelector('.restaurant_box')
-//     const restli = document.createElement('ul')
+// function displayMatches(event) {
+//   console.log('display');
+//   const matchArray = findMatches(event.target.value, data);
+//   const html = matchArray.map((place) => {
 //     const restName = place.restaurant_name;
 
-//     const restaurant_list = `
-//         <div class="box1">
-//             <li>
-//                 <div class="Rname">${restName}</div>
-//                 ${place.restaurant_street}
-//                 ${place.restaurant_town}
-//                 ${place.restaurant_zip}<br>
-//                 ${place.restaurant_phone}<br>
-//                 ${place.restaurant_email}<br>
-//             </li>
-//         </div>
-//         `;
-//     restli.innerHTML = restaurant_list;
-//     restList.append(restli);
+//     return `
+//               <div class="box2">
+//                   <ul>
+//                       <li>
+//                           <div class="Rname">${restName}</div>
+//                           ${place.restaurant_street}
+//                           ${place.restaurant_town}
+//                           ${place.restaurant_zip}<br>
+//                           ${place.restaurant_phone}<br>
+//                           ${place.restaurant_email}<br>
+//                       </li>
+//                   </ul>
+//               </div>
+//           `;
+//   }).join('');
+//   suggestions.innerHTML = html;
+// }
+// const searchInput = document.querySelector('.userform');
+// const suggestions = document.querySelector('.suggestions');
 
+// searchInput.addEventListener('change', displayMatches);
+// searchInput.addEventListener('keyup', (evt) => {
+//   evt.preventDefault();
+//   displayMatches(evt);
 // });
+// }
+// async function awardsActions() {
+// const awardResults = await getAwards();
+// console.table(awardResults);
+
+// const awardList = document.querySelector('.awards-section');
+// const html = awardResults.map((awar) => {
+//   console.log('display');
+//   return `
+//       <div class="box2">  
+//           <ul>
+//               <li>
+//                   ${awar.Awards_ID}<br>
+//                   ${awar.Award_name}<br>
+//               </li>
+//           </ul>
+//       </div>
+//       `;
+// }).join('');
+// awardList.innerHTML = html;
+// }
